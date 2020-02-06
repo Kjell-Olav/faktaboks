9f6ee83afde07896635097cc561df0c155699c71
Kjell-Olav <51406388+Kjell-Olav@users.noreply.github.com>
2020-02-06 18:28:16 +0100

    Update date.go

--- api/date.go
+++ api/date.go
@@ -7,6 +7,6 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	currentTime := time.Now().Format(time.RFC850)
-	fmt.Fprintf(w, currentTime)
+	Kjennsgjerning := "Bjørnedyr er mikroskopopiske dyr med fasong som kan minne om en bjørn. De er utbredt over hele jorden og overlever det meste. De kan gå i en slags dyp dvale de overlever nedfrysning til -200 grader Celsius, vakuum og kosmisk stråling." 
+	fmt.Fprintf(w, Kjennsgjerning)
 }
